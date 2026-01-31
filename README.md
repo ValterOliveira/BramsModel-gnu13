4a32ca8a713b2d6cb10b24463da89784991196fc
ValterOliveira <valteroliveira39@gmail.com>
2026-01-31 16:43:29 -0300

    versão com alteração de flags para trabalhar com gnu 13

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # BramsModel-gnu13
+
+Versão do modelo BRAMS 6 e Prep-chem-src 1.8.3
+
+compilado com flags para funcionar com compilador GNU 13 
+
+FLAGS PREP-CHEM-SRC(include.opt.gfortran): -std=legacy
+FLAGS BRAMS(Make_utils.in e Makefile.in): -std=legacy e  FCFLAGS=$(FC_FLAGS) -ldl -Wl,--allow-multiple-definition
+
+-Wl,--allow-multiple-definition - Foi adicionado por causa da biblioteca wgrib2 que estava apresentando conflito
---
+++ prep_chem_src/aux_src/utils/an_header.f90
@@ -0,0 +1,20 @@
+!############################# Change Log ##################################
+! 2.0.0
+!
+!###########################################################################
+!  Copyright (C)  1990, 1995, 1999, 2000, 2003 - All Rights Reserved
+!  Regional Atmospheric Modeling System - RAMS
+!###########################################################################
+
+module an_header
+
+type head_table
+   character(len=16) :: string
+   integer(kind=8) :: npointer
+   integer :: idim_type,ngrid,nvalues
+end type
+
+type (head_table), allocatable,save :: anal_table(:)
+integer, save:: nvbtab
+
+end module an_header
